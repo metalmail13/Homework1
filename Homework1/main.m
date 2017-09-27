2091b058a6eeb91f4658e6db264693e6044b9164
Administrator <administrator@Rienk-De-Jongs-MacBook-Pro.local>
2017-09-26 21:32:58 -0400

    Initial Commit

---
+++ Homework1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Homework1
+//
+//  Created by Administrator on 9/26/17.
+//  Copyright © 2017 Albert Diaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
